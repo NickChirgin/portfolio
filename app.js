ce418b8cf5f30e049890707babf4ec6aaa2f3657
Nikolay Chirgin <nickchirgin@gmail.com>
2022-05-08 12:25:35 +0300

    about me section almost done

---
+++ app.js
@@ -0,0 +1,6 @@
+const header = document.querySelectorAll('.app-content__header')
+const widgetHeader = document.querySelector("#personal")
+
+header[0].append(widgetHeader.cloneNode(true))
+header.style.width = '100%'
+console.log(header)
